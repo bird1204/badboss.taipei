a9016abf4beadf8d5ec183f1448959af3a61d063
bird Chiu <bird1204@gmail.com>
2015-12-05 16:40:50 +0800

    抓opendata的API
    IllegalBoss::Goverment::Sexsual.new(file_url: url)
    IllegalBoss::Goverment::Safety.new(file_url: url)
    IllegalBoss::Goverment::Standard.new(file_url: url)
    
    file_url可為空
    standard = IllegalBoss::Goverment::Standard.new
    standard.file_url  #輸出url
    standard.records #輸出一個array，包含所有資料

--- lib/illegal_boss/goverment/safety.rb
+++ lib/illegal_boss/goverment/safety.rb
@@ -1,6 +1,26 @@
-
 module IllegalBoss
   # Your code goes here...
-  class Goverment::Safety
+  module Goverment
+    class Safety
+      include IllegalBoss::Goverment
+      # IllegalBoss::Goverment::Safety
+      attr_accessor  :file_url
+      attr_reader :records
+
+      def initialize(attributes = {})
+        @file_url = attributes[:file_url] || 'http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=6cf496a9-7c84-4670-90ca-c329c828ea7f'
+        @records = json_to_array
+      end
+
+      private
+
+      def hash record
+        { date: record['處分日期'],
+          owner: record['負責人'],
+          name: record['事業單位名稱/自然人姓名'],
+          guilts: (record['違反法規內容'].present?) ? record['違反法規內容'].split(';') : record['違反職業安全衛生法條款'].split('及;')
+          }
+      end
+    end
   end
 end
--- lib/illegal_boss/goverment/standard.rb
+++ lib/illegal_boss/goverment/standard.rb
@@ -1,18 +1,26 @@
 module IllegalBoss
   # Your code goes here...
-  class Goverment::Standard
-    attr_accessor  :file_url
-    attr_reader :name, :owner, :date, :guilts
+  module Goverment
+    class Standard
+      include IllegalBoss::Goverment
+      # IllegalBoss::Goverment::Standard
+      attr_accessor  :file_url
+      attr_reader :records
 
-    def initialize(attributes = {})
-      @file_url = attributes[:file_url]
-      get_json(@file_url)
-    end
+      def initialize(attributes = {})
+        @file_url = attributes[:file_url] || 'http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ac5e4362-9bd6-4828-a7b9-639bd8eef84f'
+        @records = json_to_array
+      end
 
-    private
-
-    def get_json url
-      json_object = JSON.parse(open(url).read)
+      private
+      
+      def hash record
+        { date: record['處分日期'],
+          owner: record['事業單位代表人'],
+          name: record['事業單位名稱/自然人姓名'],
+          guilts: (record['違反法規內容'].present?) ? record['違反法規內容'].split(';') : record['違反勞動基準法條款'].split('及;')
+          }
+      end
     end
   end
 end
