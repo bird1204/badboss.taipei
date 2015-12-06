6c03222fddda878ec4c94ee4d2204660459f5b11
niclin <bboyceo@hotmail.com>
2015-12-06 10:25:34 +0800

    test simple form

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,2 +1,22 @@
 class CompanyController < ApplicationController
+
+  def index
+    @company = Company.all
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destory
+  end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,3 @@
 class Resource < ActiveRecord::Base
   has_many :evidence
-  has_many :guilt_resources
 end
